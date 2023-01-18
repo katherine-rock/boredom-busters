52c58743e27d72a9cbc99c011cb3ff442a8fbf47
katherine-rock <katherine_rock@hotmail.com>
2023-01-18 18:30:45 +1100

    Commented out code. Keep files to reuse logic

--- src/components/ActivityList/ActivityList.js
+++ src/components/ActivityList/ActivityList.js
@@ -1,17 +1,17 @@
-import React from "react";
-import './ActivityList.css';
-import Activity from '../Activity/Activity';
+// import React from "react";
+// import './ActivityList.css';
+// import Activity from '../Activity/Activity';
 
-const ActivityList = (props) => {
-    return (
-        <div className="ActivityList">
-            {
-            props.results.map((result, index) => {
-                return <Activity result={result} key={index} />;
-            })
-            }
-        </div>
-    )
-}
+// const ActivityList = (props) => {
+//     return (
+//         <div className="ActivityList">
+//             {
+//             props.results.map((result, index) => {
+//                 return <Activity result={result} key={index} />;
+//             })
+//             }
+//         </div>
+//     )
+// }
 
-export default ActivityList;
+// export default ActivityList; 
