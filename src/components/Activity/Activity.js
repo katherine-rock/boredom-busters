81ed61055949b8dc48ee719441ffce8a4f3ca596
katherine-rock <katherine_rock@hotmail.com>
2023-01-18 18:23:24 +1100

    Add logic to return null if no activity exists

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -3,27 +3,23 @@ import './Activity.css';
 
 const Activity = (props) => {
 
-    const activity = {
-        activity: "Kat's awesome random activity to test the component",
-        type: "social",
-        participants: 1,
-        price: 0.1,
-        link: "",
-        key: "1288934",
-        accessibility: 0.2,
-    }
+    const activityExists = true;
 
     function capitalise(string) {
         const lower = string.toLowerCase()
         return string.charAt(0).toUpperCase() + lower.slice(1) 
     }
-
-    return (
-        <div className="result">
-            <p>Activity Type: {capitalise(activity.type)}</p>
-            <p>{activity.activity}</p>
-        </div>
-    )
+    if (activityExists) {
+    // if (activityExists) {
+        return (
+            <div className="result">
+                <p>Activity Type: {capitalise(props.activity.type)}</p>
+                <p>{props.activity.activity}</p>
+            </div>
+        )
+    } else {
+        return null
+    }
 }
 
 export default Activity;
