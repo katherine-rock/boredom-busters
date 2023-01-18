29669047c02cd17cfd1f966f5fde3c9aa5be5705
katherine-rock <katherine_rock@hotmail.com>
2023-01-18 17:57:33 +1100

    Clean up naming of event handlers

--- src/components/SearchButtons/SearchButtons.js
+++ src/components/SearchButtons/SearchButtons.js
@@ -1,20 +1,6 @@
 import React from "react";
 import './SearchButtons.css';
 
-// Initial
-// const activityType = [
-//     "education", 
-//     "recreational", 
-//     "social", 
-//     "diy", 
-//     "charity", 
-//     "cooking", 
-//     "relaxation", 
-//     "music", 
-//     "busywork", 
-//     "random"
-// ]
-
 const activityType = [
     { type: "education", label: "Education"}, 
     { type: "recreational", label: "Recreation"}, 
@@ -30,10 +16,10 @@ const activityType = [
 const SearchButtons = (props) => {
     const {addResult, searchBoredAPI, chooseType} = props
 
-    function handleSearch(event) {
+    function handleClick(event) {
         console.log("Button has been clicked. This is from the handleSearch function in the SearchButtons component.")
+        console.log("This is the initial value of type: ",props.type)
         chooseType(event)
-        console.log(props.type)
         searchBoredAPI(props.type)
         console.log("This is props.type: ",props.type)
         event.preventDefault()
@@ -42,7 +28,9 @@ const SearchButtons = (props) => {
     return (
         <div className="searchButtons">
         {activityType.map((activity, index) => {
-            return <button onClick={handleSearch} key={index}>{activity.label}</button>
+            return (           
+            <button onClick={handleClick} value={activity.type} key={index}>{activity.label}</button>
+            )
         })}
         </div>
     )
