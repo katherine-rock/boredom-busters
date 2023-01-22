10f04cfe69ba9fd55a47bd9e8a9c1413a94692be
katherine-rock <katherine_rock@hotmail.com>
2023-01-22 13:01:08 +1100

    Add API Error component

---
+++ src/components/ApiError/ApiError.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './ApiError.css'
+
+export const ApiError = (props) => {
+    if (props.apiError) {
+        return (
+            <div className="ApiErrorSection">
+                <p>Oh dear!</p>
+                <p>The elders of the internet must be napping. They have not found an activity for you. Please try again</p>
+            </div>
+        )
+    } else {
+        return null
+    }
+}
+
+export default ApiError;
