0edb24064eef627ff7307f6ed3f87e4d95f79af5
katherine-rock <katherine_rock@hotmail.com>
2023-01-22 13:00:33 +1100

    Add link to API documentation to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,22 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-        <div className="footer">
-            <p>Copyright &copy; Katherine Rock 2023</p>
+        <div>
+            <div className="copyrightSection">
+                <p>Copyright &copy; Katherine Rock 2023</p>
+                <br />
+            </div>
+                
+            <div className="ApiInfoSection">
+                <p>All the activities listed here are supplied by Bored API.</p>
+                <p>You can learn more at:</p>
+                <a 
+                    href="https://www.boredapi.com/documentation"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    https://www.boredapi.com/documentation</a>
+            </div>
         </div>
     )
 }
