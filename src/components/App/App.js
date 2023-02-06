30e4b2d915a4fa436e6dd34bc5538191ead8981b
katherine-rock <katherine_rock@hotmail.com>
2023-02-06 14:26:21 +1100

    Fix issue with dependency array that was preventing automated deployment

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -36,7 +36,7 @@ const App = () => {
           } 
         )
         .catch((error) => console.log(error));
-      }},[clickCount]) 
+      }},[selectedType, clickCount]) 
       
   return (
     <div >
