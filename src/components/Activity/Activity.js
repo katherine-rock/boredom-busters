e38ec632cd8a5b0fc9491d532e7c4bb7af2dcee8
katherine-rock <katherine_rock@hotmail.com>
2023-02-07 20:25:14 +1100

    Add function to capitalise type of activity returned by API call. Add logic to always capitalise all letters in DIY

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -3,17 +3,45 @@ import './Activity.css';
 
 const Activity = (props) => {
 
-    if (props.activity) {
-        return (
-            <div className="result">
-                <p>Activity type: {props.activity.type}</p>
-                <hr/>
-                <p>{props.activity.activity}</p>
-            </div>
-        )
-    } else {
-        return null
+    // Capitalise the type of activity as returned by the API call
+    function capitalise(string) {
+        const lower = string.toLowerCase()
+        return string.charAt(0).toUpperCase() + lower.slice(1) 
     }
+
+    // if (props.activity) {
+    //     return (
+    //         <div className="result">
+    //             <p>Activity type: {capitalise(props.activity.type)}</p>
+    //             <hr/>
+    //             <p>{props.activity.activity}</p>
+    //         </div>
+    //     )
+    // } else {
+    //     return null
+    // }
+
+        if (props.activity && props.activity.type === 'diy') {
+            return (
+                    <div className="result">
+                        <p>Activity type: DIY</p>
+                        <hr/>
+                        <p>{props.activity.activity}</p>
+                    </div>
+                )
+            } if (props.activity && props.activity.type !== 'diy') {
+                return (
+                        <div className="result">
+                            <p>Activity type: {capitalise(props.activity.type)}</p>
+                            <hr/>
+                            <p>{props.activity.activity}</p>
+                        </div>
+                        )
+            } else {
+                return null
+            }
+
+    
 }
 
 export default Activity;
